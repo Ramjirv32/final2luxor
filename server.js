d4f9a852b60fedc5d1ed39aa4a1f2bfae3fa700d
ramji <ramjib2311@gmail.com>
2025-06-22 20:56:22 +0530

    vercel-added-api

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const photoGalleryRoutes = require('./routes/photoGalleryRoutes');
 const newsletterRoutes = require('./routes/newsletterRoutes');
 
 const app = express();
-app.use(cors());
+app.use(cors('*'),{
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
